1086cea94684296484ec7b0c3d6cdd86129ebbbc
jescalante02 <55394334+jescalante02@users.noreply.github.com>
2021-04-05 00:47:29 -0500

    Update UserAPI.js

--- client/src/api/UserAPI.js
+++ client/src/api/UserAPI.js
@@ -5,7 +5,7 @@ function UserAPI(token) {
     const [isLogged, setIsLogged] = useState(false)
     const [isAdmin, setIsAdmin] = useState(false)
     const [cart, setCart] = useState([])
-    const [history, setHistory] = useState([])
+    const [history, setHistory] = useState([]) 
 
     useEffect(() =>{
         if(token){
