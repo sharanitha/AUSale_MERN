d107641623d5b4027233f5e6c59885bf49269a6a
sharanitha <55889946+sharanitha@users.noreply.github.com>
2021-04-07 21:49:10 -0500

    removed product_id var

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -70,11 +70,13 @@ const productCtrl = {
             const {product_id, title, price, description, content, images, category} = req.body;
             if(!images) return res.status(400).json({msg: "No image upload"})
 
+            /*
             //check if product already exists
             const product = await Products.findOne({product_id})
             if(product){
                 return res.status(400).json({msg: "This product already exists."})
             }
+            */
 
             const newProduct = new Products({
                 product_id, title: title.toLowerCase(), price, description, content, images, category
